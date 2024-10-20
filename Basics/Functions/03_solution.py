94716131e477b4f042004cbb9bfdf7ca99baa01a
Aatif <aatifkhan2001@gmail.com>
2024-10-20 10:07:42 +0530

    added a new file

---
+++ Basics/Functions/03_solution.py
@@ -0,0 +1,10 @@
+# 3. Polymorphism in Functions
+# Problem: Write a function multiply that multiplies two numbers, but can also accept and multiply strings.
+
+def Multiply(a,b):
+    return a*b
+
+
+print(Multiply(8,5))
+print(Multiply('a',5))
+print(Multiply(5,'a'))
