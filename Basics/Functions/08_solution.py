e679f8f92d20715375502984226d26c5d041743e
Aatif <aatifkhan2001@gmail.com>
2024-10-23 12:08:14 +0530

    added a new file

---
+++ Basics/Functions/08_solution.py
@@ -0,0 +1,10 @@
+# 8. Function with **kwargs
+# Problem: Create a function that accepts any number of keyword arguments and prints them in the format key: value.
+def print_kwargs(**kwargs):
+    for key, value in kwargs.items():
+        print(f"{key}: {value}")
+
+
+print_kwargs(name="foo", power="bar")
+print_kwargs(name="foo")
+print_kwargs(name="foo", power="lazer", enemy = "bar")
